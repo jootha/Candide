53386a1eacf5c35ce9a39d10d6aba781fbbb6a5b
jootha <lecomteadrien@hotmail.fr>
2025-09-15 16:53:55 +0200

    Struct et data

--- Candide/Data/Database.swift
+++ Candide/Data/Database.swift
@@ -6,3 +6,105 @@
 //
 
 import Foundation
+
+
+// Liste des plantes
+var plants: [Plant] = [
+    Plant(
+        name: "Ficus",
+        soilType: .wellDrained,
+        watering: .weekly,
+        sunlight: .indirectLight,
+        isIndoor: true,
+        plantTask: [] ),
+    Plant(
+        name: "Lavande",
+        soilType: .calcareous,
+        watering: .every10Days,
+        sunlight: .fullSun,
+        isIndoor: false,
+        plantTask: [] ),
+    Plant(
+        name: "Aloe Vera",
+        soilType: .cactusMix,
+        watering: .biweekly,
+        sunlight: .indirectLight,
+        isIndoor: true,
+        plantTask: [] ),
+    Plant(
+        name: "Basilic",
+        soilType: .rich,
+        watering: .every2Days,
+        sunlight: .fullSun,
+        isIndoor: false,
+        plantTask: [] ),
+    Plant(
+        name: "Monstera",
+        soilType: .wellDrained,
+        watering: .weekly,
+        sunlight: .mediumLight,
+        isIndoor: true,
+        plantTask: [] ),
+    Plant(
+        name: "Romarin",
+        soilType: .dry,
+        watering: .weekly,
+        sunlight: .fullSun,
+        isIndoor: false,
+        plantTask: [] ) ]
+
+//Liste des tâches
+var tasks: [Task] = [
+    Task(name: "Arroser", date: "26/01/2025", isDone: false),
+    Task(name: "Rajouter de l'engrais", date: "26/01/2025", isDone: true),
+    Task(name: "Tailler", date: "26/01/2025", isDone: false) ]
+
+//Liste des postes
+var posts: [Post] = [
+    Post(
+        title: "Comment arroser vos plantes efficacement",
+        image: "plant.fill",
+        contentText: "L'arrosage est crucial pour la santé de vos plantes...",
+        description: "Conseils d’arrosage pour plantes d’intérieur",
+        author: users[3],
+        date: "26/01/2025",
+        nbLike: 42,
+        comments: comments[2] ),
+    Post(
+        title: "DIY : Construire une jardinière en bois",
+        image: "hammer.fill",
+        contentText: "Voici comment construire une belle jardinière en quelques étapes...",
+        description: "Tutoriel pour fabriquer une jardinière simple",
+        author: users[2],
+        date: "26/01/2025",
+        nbLike: 29,
+        comments: comments[0] ),
+    Post(
+        title: "Les meilleures plantes dépolluantes",
+        image: "leaf.fill",
+        contentText: "Certaines plantes peuvent améliorer la qualité de l'air chez vous...",
+        description: "Top 5 des plantes purificatrices",
+        author: users[1],
+        date: "26/01/2025",
+        nbLike: 78,
+        comments: comments[2] ) ]
+
+//Liste des commentaires
+var comments = [ Comment(
+    commentText: "Super article, merci !",
+    date: "26/01/2025",
+    author: users[0]),
+                 Comment(
+    commentText: "Très bien écrit 👏",
+    date: "26/01/2025",
+    author: users[1]),
+                 Comment(
+    commentText: "J’ai hâte de lire la suite.",
+    date: "26/01/2025",
+    author: users[0]) ]
+
+//Liste des profils utilisateurs
+var users = [Profile(username: "alice_garden", profilePic: "person.circle"),
+             Profile(username: "bob_the_planter", profilePic: "hammer.fill"),
+             Profile(username: "clara_leaf", profilePic: "paintbrush.fill") ]
+
--- Candide/Data/Models.swift
+++ Candide/Data/Models.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+
+//Enums de la structure de plantes
+enum SoilType: String {
+    case wellDrained = "Bien drainé"
+    case sandy = "Sableux"
+    case rich = "Riche et humide"
+    case dry = "Sec"
+    case calcareous = "Calcaire"
+    case cactusMix = "Cactus"
+}
+
+enum WateringFrequency: String {
+    case daily = "Tous les jours"
+    case every2Days = "Tous les 2 jours"
+    case weekly = "1 fois par semaine"
+    case biweekly = "Toutes les 2 semaines"
+    case every10Days = "Tous les 10 jours"
+}
+
+enum Sunlight: String {
+    case fullSun = "Plein soleil"
+    case indirectLight = "Lumière indirecte"
+    case mediumLight = "Lumière moyenne"
+    case shade = "Ombre"
+}
+
+//Structure de plantes
+struct Plant : Identifiable {
+    var id = UUID()
+    var name: String
+    var soilType: SoilType
+    var watering: WateringFrequency
+    var sunlight: Sunlight
+    var isIndoor: Bool
+    var plantTask: [Task]
+}
+
+//Structure de tâches
+struct Task: Identifiable {
+    var id = UUID()
+    var name: String
+    var date: String
+    var isDone: Bool
+}
+
+//Structure de posts
+struct Post: Identifiable {
+    var id = UUID()
+    var title: String
+    var image: String
+    var contentText: String
+    var description: String
+    var author: Profile
+    var date: String
+    var nbLike: Int
+    var comments: Comment
+}
+
+//Structure de commentaires
+struct Comment: Identifiable {
+    var id = UUID()
+    var commentText: String
+    var date: String
+    var author: Profile
+}
+
+//Structure de profil utilisateur
+struct Profile: Identifiable {
+    var id = UUID()
+    var username: String
+    var profilePic: String
+}
+
+struct Models: View {
+    var body: some View {
+        
+    }
+}
