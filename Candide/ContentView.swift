2d2567a0dfa3b9c08eb898f0b3d9b416eca1487e
jootha <lecomteadrien@hotmail.fr>
2025-09-15 15:54:34 +0200

    UI Jardin couleurs basiques

--- Candide/ContentView.swift
+++ Candide/ContentView.swift
@@ -13,7 +13,35 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Jardin")
+            
+            ZStack{
+                Color.green.ignoresSafeArea()
+                
+                VStack {
+                    ZStack{
+                        Rectangle().fill(Color.yellow)
+                            .frame(width: 350, height: 200)
+                            .cornerRadius(16)
+                            .padding()
+                        
+                        Rectangle()
+                            .foregroundStyle(.blue)
+                            .frame(width: 300, height: 150)
+                        
+                        HStack{
+                            Spacer()
+                            ZStack{
+                                Circle().frame(width: 30)
+                                    .opacity(0.60)
+                                    .foregroundColor(.gray)
+                                Image(systemName: "heart.fill")
+                            }
+                            Spacer()
+                        }.frame(maxWidth: 350)
+                    }
+                }
+            }
         }
         .padding()
     }
