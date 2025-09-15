c767d7e885b2ded705a139c4a7a27ef2ac2467c7
jootha <lecomteadrien@hotmail.fr>
2025-09-15 15:00:26 +0200

    initial commit

---
+++ Candide/CandideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandideApp.swift
+//  Candide
+//
+//  Created by apprenant84 on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CandideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
