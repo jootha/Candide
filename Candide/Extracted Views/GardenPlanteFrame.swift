2b61d6b772224712423e8102ae7b4a66ae5ab8bd
jootha <lecomteadrien@hotmail.fr>
2025-09-16 14:07:48 +0200

    extracted views

---
+++ Candide/Extracted Views/GardenPlanteFrame.swift
@@ -0,0 +1,18 @@
+//
+//  GardenPlanteFrame.swift
+//  Candide
+//
+//  Created by apprenant84 on 16/09/2025.
+//
+
+import SwiftUI
+
+struct GardenPlanteFrame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GardenPlanteFrame()
+}
