2a1c5e651cd0cfd1bbb3eab941f76c2b3c7dd71b
jootha <lecomteadrien@hotmail.fr>
2025-09-24 15:27:50 +0200

    fix image addplant view visualizer

--- Candide/Screens/AddPlantView.swift
+++ Candide/Screens/AddPlantView.swift
@@ -25,127 +25,132 @@ struct AddPlantView: View {
         ZStack {
             Color.cGreen.ignoresSafeArea()
 
-            HStack {
-                Spacer()
-                //  INFORMATIONS
-                VStack {
-                    HStack {
-                        Text("Informations: ")
-                            .font(.headline)
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 6)
-                            .background(Color.cYellow)
-                            .cornerRadius(12)
-                            .shadow(radius: 2)
-                            .padding(.horizontal, 30)
-                            .shadow(radius: 2)
-
-                        Spacer()
-                    }
-                    .padding(.top, 16)
-
-                    ScrollView(showsIndicators: false) {
-                        VStack {
-                            TextField("**Nom**", text: $name)
-                                .frame(height: 10, alignment: .leading)
-                                .padding()
-                                .background(Color.cPink)
-                                .cornerRadius(8)
-                                .padding(.horizontal)
-                                .foregroundStyle(.cDarkBlue)
-                            
-                            Picker("Ensoleillement", selection: $sunlight) {
-                                ForEach(Sunlight.allCases, id: \.self) {
-                                    light in
-                                    Text(light.rawValue)
-                                }
-                            }.frame(height: 10, alignment: .leading)
-                                .padding()
-                                .background(Color.cPink)
-                                .cornerRadius(8)
-                                .padding(.horizontal)
-                                .foregroundStyle(.cDarkBlue)
-
-                            Picker("Type de sol", selection: $soilType) {
-                                ForEach(SoilType.allCases, id: \.self) { soil in
-                                    Text(soil.rawValue)
-                                }
-                            }.frame(height: 10, alignment: .leading)
-                                .padding()
-                                .background(Color.cPink)
-                                .cornerRadius(8)
-                                .padding(.horizontal)
-                                .foregroundStyle(.cDarkBlue)
-                            Picker("Arrosage", selection: $watering) {
-                                ForEach(WateringFrequency.allCases, id: \.self)
-                                {
-                                    water in
-                                    Text(water.rawValue)
-                                }
-                            }.frame(height: 10, alignment: .leading)
-                                .padding()
-                                .background(Color.cPink)
-                                .cornerRadius(8)
-                                .padding(.horizontal)
-                                .foregroundStyle(.cDarkBlue)
-                            HStack {
-                                Toggle("En Interieur?", isOn: $isIndoor)
-                            }.frame(height: 10, alignment: .leading)
-                                .padding()
-                                .background(Color.cPink)
-                                .cornerRadius(8)
-                                .padding(.horizontal)
-                                .foregroundStyle(.cDarkBlue)
-                        }
-                        .padding(.horizontal, 8)
-                        .padding(.vertical, 16)
+            VStack {
+                HStack {
+                    Text("Informations: ")
+                        .font(.headline)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
                         .background(Color.cYellow)
-                        .cornerRadius(16)
+                        .cornerRadius(12)
                         .shadow(radius: 2)
-                        //if let image = photoViewModel.photo {
-                            Image("A")
+                        .padding(.horizontal, 30)
+                        .shadow(radius: 2)
+
+                    Spacer()
+                }
+                .padding(.top, 16)
+
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 16) {
+                        TextField("**Nom**", text: $name)
+                            .padding()
+                            .frame(height: 50)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.cPink)
+                            .cornerRadius(8)
+                            .foregroundStyle(.cDarkBlue)
+                            .padding(.horizontal, 30)
+
+                        Picker("Ensoleillement", selection: $sunlight) {
+                            ForEach(Sunlight.allCases, id: \.self) { light in
+                                Text(light.rawValue)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .padding()
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.cPink)
+                        .cornerRadius(8)
+                        .foregroundStyle(.cDarkBlue)
+                        .padding(.horizontal, 30)
+
+                        Picker("Type de sol", selection: $soilType) {
+                            ForEach(SoilType.allCases, id: \.self) { soil in
+                                Text(soil.rawValue)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .padding()
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.cPink)
+                        .cornerRadius(8)
+                        .foregroundStyle(.cDarkBlue)
+                        .padding(.horizontal, 30)
+
+                        Picker("Arrosage", selection: $watering) {
+                            ForEach(WateringFrequency.allCases, id: \.self) {
+                                water in
+                                Text(water.rawValue)
+                            }
+                        }
+                        .pickerStyle(.menu)
+                        .padding()
+                        .frame(height: 50)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.cPink)
+                        .cornerRadius(8)
+                        .foregroundStyle(.cDarkBlue)
+                        .padding(.horizontal, 30)
+
+                        Toggle("En intérieur ?", isOn: $isIndoor)
+                            .padding()
+                            .frame(height: 50)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .background(Color.cPink)
+                            .cornerRadius(8)
+                            .foregroundStyle(.cDarkBlue)
+                            .padding(.horizontal, 30)
+
+                        if let uiImage = photoViewModel.photo {
+                            Image(uiImage: uiImage)
                                 .resizable()
                                 .scaledToFit()
                                 .frame(width: 350, height: 350)
-                        //}
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal, 30)
-                    
-                    HStack {
-                        Spacer()
-                        Button {
-                            showCamera = true
-                        } label: {
-                            Label("Plus", systemImage: "photo")
-                                .labelStyle(.iconOnly)
-                                .padding(16)
-                                .background(.cDarkBlue)
-                                .foregroundStyle(.cOrange)
-                                .cornerRadius(32)
-                                .font(.system(size: 32))
-                                .bold()
-                        }.sheet(isPresented: $showCamera) {
-                            CameraScreen(viewModel: photoViewModel)
+                                .clipped()
+                                .cornerRadius(12)
+                                .onTapGesture {
+                                    selectedPhoto = uiImage
+                                    showFullScreen = true
+                                }
                         }
-                        Spacer()
+                        if selectedPhoto != nil {
+                            Image(uiImage: selectedPhoto!)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 1, height: 1)
+                        }
+
+                        HStack {
+                            Spacer()
+                            Button {
+                                showCamera = true
+                            } label: {
+                                Label("Plus", systemImage: "photo")
+                                    .labelStyle(.iconOnly)
+                                    .padding(16)
+                                    .background(.cDarkBlue)
+                                    .foregroundStyle(.cOrange)
+                                    .cornerRadius(32)
+                                    .font(.system(size: 32))
+                                    .bold()
+                            }
+                            .sheet(isPresented: $showCamera) {
+                                CameraScreen(viewModel: photoViewModel)
+                            }
+                            Spacer()
+                        }
+                        .padding(.vertical, 20)
                     }
-                }
-                .scrollContentBackground(.hidden)
-                .background(
-                    Color.cYellow.opacity(0.3),
-                    in: RoundedRectangle(cornerRadius: 12)
-                )
-                
-                
-                if selectedPhoto != nil {
-                    Image(uiImage: selectedPhoto!)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 1, height: 1)
+                    .padding(.vertical, 16)
+                    .background(Color.cYellow)
+                    .cornerRadius(16)
+                    .shadow(radius: 2)
+                    .padding(.horizontal, 30)
                 }
             }
-            Spacer()
         }
 
         .toolbar {
@@ -197,7 +202,6 @@ struct AddPlantView: View {
             }
         }
         .navigationTitle("Nouvelle plante")
-
     }
 }
 
