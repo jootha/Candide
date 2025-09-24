0ca8b662c99ccfe3233d7993a282bf4dd884fa79
jootha <lecomteadrien@hotmail.fr>
2025-09-24 10:23:05 +0200

    fix addplantview

--- Candide/Screens/AddPlantView.swift
+++ Candide/Screens/AddPlantView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct AddPlantView: View {
-    @FocusState private var isTextFieldFocused: Bool
     @State private var name: String = ""
     @State private var soilType: SoilType = .wellDrained
     @State private var watering: WateringFrequency = .daily
@@ -23,73 +22,42 @@ struct AddPlantView: View {
     @State private var showFullScreen = false
 
     var body: some View {
-        ZStack {
-            Color.gray.opacity(0.1)
             ZStack {
                 Form {
                     VStack {
                         TextField("**Nom**", text: $name)
-                            .focused($isTextFieldFocused)
-                            .onAppear {self.isTextFieldFocused = true}
-                        
                         //  IMAGES??t???
-                        
                         Picker("Ensoleillement", selection: $sunlight) {
                             ForEach(Sunlight.allCases, id: \.self) { light in
                                 Text(light.rawValue)
                             }
                         }
-                        
                         Picker("Type de sol", selection: $soilType) {
                             ForEach(SoilType.allCases, id: \.self) { soil in
                                 Text(soil.rawValue)
                             }
                         }
-                        
                         Picker("Arrosage", selection: $watering) {
-                            ForEach(WateringFrequency.allCases, id: \.self) { water in
+                            ForEach(WateringFrequency.allCases, id: \.self) {
+                                water in
                                 Text(water.rawValue)
                             }
                         }
-                        
                         Toggle("En Interieur?", isOn: $isIndoor)
-                        
-                        ForEach(photoViewModel.photos.indices, id: \.self) { index in
-                            Image(uiImage: photoViewModel.photos[index])
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 100, height: 100)
-                                .clipped()
-                                .cornerRadius(12)
-                        }
+                        Image(uiImage: photoViewModel.photo)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 100, height: 100)
+                            .clipped()
+                            .cornerRadius(12)
                     }
-                    .background(Color.cYellow.opacity(0.3), in: RoundedRectangle(cornerRadius: 12))
+                    .background(
+                        Color.cYellow.opacity(0.3),
+                        in: RoundedRectangle(cornerRadius: 12)
+                    )
                 }.scrollContentBackground(.hidden)
 
             }
-            .toolbar {
-                ToolbarItem(placement: .confirmationAction) {
-                    Button("Enregistrer") {
-                        let newPlant = Plant(
-                            name: name,
-                            soilType: soilType,
-                            watering: watering,
-                            sunlight: sunlight,
-                            isIndoor: isIndoor,
-                            note:"..."
-                        )
-                        
-                        onSave(newPlant)
-                        navPath.removeLast()
-                    }
-                    .disabled(name.trimmingCharacters(in: .whitespaces).isEmpty)
-                    .tint(.white)
-
-                }
-
-            }
-            .navigationTitle("Nouvelle plante")
-            
             Image(uiImage: photoViewModel.photo)
                 .resizable()
                 .scaledToFill()
@@ -99,14 +67,28 @@ struct AddPlantView: View {
                 .onTapGesture {
                     selectedPhoto = photoViewModel.photo
                     showFullScreen = true
-            }
+                }
             if selectedPhoto != nil {
                 Image(uiImage: selectedPhoto!)
                     .resizable()
                     .scaledToFill()
                     .frame(width: 1, height: 1)
             }
-        }
+            //Ajout Image
+            Button {
+                showCamera = true
+            } label: {
+                Label("Plus", systemImage: "photo")
+                    .labelStyle(.iconOnly)
+                    .padding(16)
+                    .background(.cDarkBlue)
+                    .foregroundStyle(.cOrange)
+                    .cornerRadius(32)
+                    .font(.system(size: 32))
+                    .bold()
+            }.sheet(isPresented: $showCamera) {
+                CameraScreen(viewModel: photoViewModel)
+            }
         .toolbar {
             ToolbarItem(placement: .confirmationAction) {
                 Button("Enregistrer") {
@@ -116,7 +98,8 @@ struct AddPlantView: View {
                         soilType: soilType,
                         watering: watering,
                         sunlight: sunlight,
-                        isIndoor: isIndoor
+                        isIndoor: isIndoor,
+                        note: "..."
                     )
 
                     onSave(newPlant)
@@ -155,22 +138,6 @@ struct AddPlantView: View {
             }
         }
         .navigationTitle("Nouvelle plante")
-
-        //Ajout Image
-        Button {
-            showCamera = true
-        } label: {
-            Label("Plus", systemImage: "photo")
-                .labelStyle(.iconOnly)
-                .padding(16)
-                .background(.cDarkBlue)
-                .foregroundStyle(.cOrange)
-                .cornerRadius(32)
-                .font(.system(size: 32))
-                .bold()
-        }.sheet(isPresented: $showCamera) {
-            CameraScreen(viewModel: photoViewModel)
-        }
     }
 }
 
